4bb42edb984137ed0f1cd04c882bc30b5f908c44
Chikis <sebastiansilverioevelyn@gmail.com>
2026-01-25 19:26:53 -0600

    primer comit

---
+++ app.js
@@ -0,0 +1,9 @@
+'use strict';
+
+// Declaración de utilidades y referencias
+const $ = (sel, root = document) => root.querySelector(sel);
+const $$ = (sel, root = document) => Array.from(root.querySelectorAll(sel));
+
+const estadoUI = $('#estadoUI');
+const setEstado = (msg) => { estadoUI.textContent = msg; };
+setEstado('Listo');
